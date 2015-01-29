0dfd7fd0a84fac85ff6b06af55fb29c5ef57f953
Amy Worrell <amworrel@indiana.edu>
2015-01-29 17:33:15 -0500

    Initial Commit

---
+++ TutorAppCapstone/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TutorAppCapstone
+//
+//  Created by Worrell, Amy Marie on 1/29/15.
+//  Copyright (c) 2015 Worrell, Amy Marie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
