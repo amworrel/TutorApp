7a52fc2c6e4018508038c5af432e1f9998cd39a9
Amy Worrell <amworrel@indiana.edu>
2015-02-17 16:06:25 -0500

    dfasf

--- TutorAppCapstone/LogInViewController.m
+++ TutorAppCapstone/LogInViewController.m
@@ -33,7 +33,7 @@
 
 -(void)loginViewFetchedUserInfo:(FBLoginView *)loginView user:(id<FBGraphUser>)user{
     
-    self.profilePictureView.profileID = user.id;
+    self.profilePictureView.profileID = user.objectID;
     SelectionViewController.FBnameLabel.text = user.name;
     NSLog(@"%@", user.name);
 }
