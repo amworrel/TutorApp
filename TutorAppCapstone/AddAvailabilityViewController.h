fa71dad20398159700cfc714f4ecd57d3e0e044d
Joshua Mizraji <jomizraj@indiana.edu>
2015-03-24 15:42:05 -0400

    UI with buttons

---
+++ TutorAppCapstone/AddAvailabilityViewController.h
@@ -0,0 +1,42 @@
+//
+//  AddAvailabilityViewController.h
+//  TutorAppCapstone
+//
+//  Created by Mizraji, Joshua Alan on 3/24/15.
+//  Copyright (c) 2015 Worrell, Amy Marie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddAvailabilityViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *addMonth;
+
+@property (weak, nonatomic) IBOutlet UITextField *addDay;
+
+@property (weak, nonatomic) IBOutlet UITextField *addYear;
+
+@property (weak, nonatomic) IBOutlet UITextField *addStartHour;
+
+@property (weak, nonatomic) IBOutlet UITextField *addStartMinutes;
+
+@property (weak, nonatomic) IBOutlet UITextField *addStartPMAM;
+
+@property (weak, nonatomic) IBOutlet UITextField *addEndHour;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *addEndMinutes;
+
+@property (weak, nonatomic) IBOutlet UITextField *addEndPMAM;
+
+
+@property (weak, nonatomic) IBOutlet UITextField *addLocation;
+
+@property (weak, nonatomic) IBOutlet UITextField *addRate;
+
+@property (weak, nonatomic) IBOutlet UITextView *addDetails;
+
+
+
+
+@end
---
+++ TutorAppCapstone/UpdateAvailabilityViewController.h
@@ -0,0 +1,20 @@
+//
+//  UpdateAvailabilityViewController.h
+//  TutorAppCapstone
+//
+//  Created by Mizraji, Joshua Alan on 3/24/15.
+//  Copyright (c) 2015 Worrell, Amy Marie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpdateAvailabilityViewController : UIViewController
+
+
+
+
+@property (weak, nonatomic) IBOutlet UITableView *updateAvailTableView;
+
+
+
+@end
